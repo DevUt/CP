ec9a1b1b66595f172a0051e068aa1b1823f9255d
Utkarsh Tiwari <utkarsh02t@gmail.com>
2024-07-17 00:06:05 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
 #! /bin/bash
 
+usage() { echo "$0 dest_folder dest_sturcture"; exit ; }
+
+while getopts ":h" arg; do
+   case $arg in
+      h)
+         usage
+         ;;
+   esac
+done
+
+if [[ $# -ne 2 ]] ; then
+    echo 'Unexpectable number of arguments'
+    exit 1
+fi
+
 COMPILE_FLAG_DIR="/home/devut/Projects/CP/boilers/"
 COMPILE_FLAG_FILE="compile_flags.txt"
 
@@ -7,10 +22,12 @@ dest_folder=$1
 folder_struct=$2
 
 dest_folder=`realpath $1`
+
 final_folder="${dest_folder}/${folder_struct}"
 mkdir -p ${final_folder}
 if [ $? -ne 0 ] ; then
    echo "Failure creating Directory!"
+   exit
 fi
 
 
