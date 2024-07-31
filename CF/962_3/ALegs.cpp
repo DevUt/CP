f9219c6dc116b2f58164baa73a55fc85256282c2
Utkarsh Tiwari <utkarsh02t@gmail.com>
2024-07-31 15:00:55 +0530

    Update

---
+++ CF/962_3/ALegs.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+// #pragma GCC target("fpmath=387")
+// Use some pragma optimizations
+#pragma GCC target(                                                            \
+    "sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+#pragma GCC optimize("tree-vectorize") // DO NOT USE ON USACO
+// #pragma GCC optimize("Ofast")
+// #define SLOWIO
+// Shotern long variable names
+using ull = unsigned long long;
+using ll = long long;
+using ldb = long double;
+#define MOD 1000000007
+// Operations on bits
+#define popcnt(x) __builtin_popcount(x)
+#define popcntll(x) __builtin_popcountll(x)
+#define trailz(x) __builtin_ctz(x)
+#define trailzll(x) __builtin_ctzll(x)
+#define leadz(x) __builtin_clz(x) // REMEMBER THE X SHOULD BE UNSIGNED
+#define leadzll(x) __builtin_clzll(x)
+#define oddparity(x) __builtin_parity(x)
+#define findFirstSet(x) __builtin_ffs(x) // index of the least significant bits of x) + 1.
+
+// The namespaces
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define uniq(x) x.erase(unique(all(x)), x.end());
+template <typename T> istream &operator>>(istream &is, vector<T> &v) {
+  for (auto &i : v)
+    is >> i;
+  return is;
+}
+template <typename T> ostream &operator<<(ostream &os, vector<T> v) {
+  for (auto &i : v)
+    os << i << ' ';
+  return os;
+}
+template <typename T, size_t sz>
+istream &operator>>(istream &is, array<T, sz> &v) {
+  for (auto &i : v)
+    is >> i;
+  return is;
+}
+template <typename T, size_t sz>
+ostream &operator<<(ostream &os, array<T, sz> v) {
+  for (auto &i : v)
+    os << i << ' ';
+  return os;
+}
+
+template <class T> using MinHeap = priority_queue<T, vector<T>, greater<T>>;
+template <class T> using MaxHeap = priority_queue<T, vector<T>, less<T>>;
+
+template <typename I> struct _reversed_struct {
+  I &v_;
+  explicit _reversed_struct(I &v) : v_{v} {}
+  typename I::reverse_iterator begin() const { return v_.rbegin(); }
+  typename I::reverse_iterator end() const { return v_.rend(); }
+};
+template <typename I> _reversed_struct<I> reversed(I &v) {
+  return _reversed_struct<I>(v);
+}
+template <typename T> void ckmin(T &a, const T &b) { a = min(a, b); }
+template <typename T> void ckmax(T &a, const T &b) { a = max(a, b); }
+template <typename T, typename P> T cceil(const T &a, const P &b) {
+  ll x = a, y = b;
+  return (x + y - 1) / y;
+}
+template <typename T, typename P> T cfloor(const T &a, const P &b) {
+  ll n = a, d = b;
+  return n >= 0 ? n / d : -1 - (-1 - n) / d;
+}
+
+void solve(int test_case) {
+  int n; cin>>n;
+  if(n%4 == 0){
+    cout<<n/4<<'\n';
+  }else{
+    cout<<n/4 + 1<<'\n';
+  }
+  // cout<<"Case #"<<test_case<<": ";
+}
+
+signed main() {
+#ifndef SLOWIO
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+#endif
+  // DON"T DO THE STRING STREAM STUFF
+
+#ifndef UTKARSH_LOCAL
+// freopen("problemname.in", "r", stdin);
+// // the following line creates/overwrites the output file
+// freopen("problemname.out", "w", stdout);
+#endif
+  int test_case = 1;
+  cin >> test_case;
+  for (int i = 1; i <= test_case; i++) {
+    solve(i);
+  }
+}
---
+++ CF/Pinely_R4_1_2/Cgen.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <random>
+
+using ull = unsigned long long;
+using ll = long long;
+using ldb = long double;
+
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+template <typename T, typename P> T cceil(const T &a, const P &b) {
+  ll x = a, y = b;
+  return (x + y - 1) / y;
+}
+template <typename T, typename P> T cfloor(const T &a, const P &b) {
+  ll n = a, d = b;
+  return n >= 0 ? n / d : -1 - (-1 - n) / d;
+}
+
+mt19937 initialize_rng(){ // Note this is not the static version
+  random_device rnd_device;
+  static mt19937 rng {rnd_device()};
+  return rng;
+}
+
+vector<int> generate_random_vector(size_t size, int start, int finish){
+  vector<int> ret(size);
+  mt19937 rng = initialize_rng();
+  uniform_int_distribution<int> dist {start, finish};
+  auto gen = [&](){
+    return dist(rng);
+  };
+  generate(all(ret),gen);
+  return ret;
+}
+
+int generate_random_num(int start, int finish){
+  mt19937 rng = initialize_rng();
+  uniform_int_distribution<int>  dist{start, finish};
+  return dist(rng);
+}
+
+void solve(int test_case) {
+  int n = generate_random_num(0, 10);
+  cout<<n<<'\n';
+  vector v = generate_random_vector(n,0,100);
+  for(auto& x : v)
+    cout<<x<<' ';
+  cout<<'\n';
+  // cout<<"Case #"<<test_case<<": ";
+}
+
+signed main() {
+#ifndef SLOWIO
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+#endif
+  // DON"T DO THE STRING STREAM STUFF
+
+#ifndef UTKARSH_LOCAL
+// freopen("problemname.in", "r", stdin);
+// // the following line creates/overwrites the output file
+// freopen("problemname.out", "w", stdout);
+#endif
+  initialize_rng();
+  int test_case = 1;
+  cin >> test_case;
+  for (int i = 1; i <= test_case; i++) {
+    solve(i);
+  }
+}
